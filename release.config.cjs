a4392a206abf6fe56d7730165337e6e1c2a359de
calebosam <caleb.osam@amalitech.com>
2023-12-06 16:57:02 +0000

    feat: add release configs

---
+++ release.config.cjs
@@ -0,0 +1,29 @@
+const config = {
+  branches: ["staging"],
+  plugins: [
+    [
+      "@semantic-release/commit-analyzer",
+      {
+        preset: "conventionalcommits",
+        parserOpts: {
+          noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES"],
+        },
+        releaseRules: [
+          { type: "breaking_change", release: "major" },
+        ],
+      },
+    ],
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/git",
+      {
+        assets: ["dist/*.js", "dist/*.js.map", "CHANGELOG.md"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+    "@semantic-release/github",
+  ],
+};
+
+module.exports = config;
