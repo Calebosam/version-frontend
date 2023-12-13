2f9292359c3a465d7aef0aa069f6bebe75407f31
calebosam <caleb.osam@amalitech.com>
2023-12-13 11:56:17 +0000

    breaking_change: removed some features

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,8 @@ function App() {
 
   return (
     <>
-      <h1>Feat 1.0</h1>
-      <h1>Feat 2.0</h1>
-      <p>Breaking Change 1</p>
-      <p>Breaking Change 1</p>
-      <p>Breaking Change 1</p>
-      <p>Breaking Change 1</p>
-
+      <h1>Feat 1</h1>
+      <h1>Feat 2</h1>
     </>
   )
 }
